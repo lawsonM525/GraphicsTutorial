24d277d1aeb0873bab9db2d119b91b5134e84d7f
Michelle L <mlawson@smith.edu>
2023-03-21 22:47:48 -0400

    completed lab

--- MyPanel.java
+++ MyPanel.java
@@ -1,9 +1,13 @@
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
 
 public class MyPanel extends JPanel{
+
+    Image image;
     
     MyPanel(){
+
+        image = new ImageIcon("woman.png").getImage();
         this.setPreferredSize(new Dimension(500, 500));
     }
 
@@ -11,8 +15,10 @@ public class MyPanel extends JPanel{
         
         Graphics2D g2D = (Graphics2D) g;//cast g to Graphics2D
 
+        g2D.drawImage(image, 0, 0, null);
+
         g2D.setPaint(Color.blue);//sets the color of the line to blue
-        g2D.setStroke(new BasicStroke(5));//sets the width of the line to 5 pixels
+        //g2D.setStroke(new BasicStroke(5));//sets the width of the line to 5 pixels
         //g2D.drawLine(0, 0, 500, 500);
 
         //g2D.setPaint(Color.pink);
@@ -20,6 +26,24 @@ public class MyPanel extends JPanel{
         //g2D.fillRect(0,0, 100, 200);
 
         g2D.setPaint(Color.orange);
-        g2D.drawOval(0,0,100,100);
+        //g2D.drawOval(0,0,100,100);
+        //g2D.fillOval(0,0,100,100);
+
+        g2D.setPaint(Color.red);
+        g2D.fillArc(0,0,100,100,0,180);
+        g2D.setPaint(Color.white);
+        g2D.fillArc(0,0,100,100,180,180);
+
+        int[] xPoints = {150,250,350};
+        int[] yPoints = {300,150,300};
+        g2D.setPaint(Color.yellow);
+        g2D.drawPolygon(xPoints, yPoints, 3);
+        g2D.fillPolygon(xPoints, yPoints, 3);
+
+        // g2D.setPaint(Color.pink);
+        // g2D.setFont(new Font ("Ink Free", Font.BOLD, 50));
+        // g2D.drawString("U R A WINNER!", 50, 50);
+
+        
     }
 }
